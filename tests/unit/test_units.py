9a68e75629d6d07ef8766adda9ed93b2a3cdfc60
Logi Leifsson <logileifs@gmail.com>
2017-10-29 22:10:14 +0000

    added more tests

--- tests/unit/test_units.py
+++ tests/unit/test_units.py
@@ -2,10 +2,12 @@
 # -*- coding: utf-8 -*-
 
 import yaml
+import os
 
 from testindicator import paths
 from testindicator import config as cfg
 from testindicator import application as app
+from testindicator import fsmonitor as fsmon
 
 
 class TestUnits():
@@ -35,3 +37,22 @@ class TestUnits():
 
 	def test_add_new_test(self):
 		assert True
+
+	def test_ignore(self):
+		#handler = MyHandler(None)
+		#print('ROOT_PATH: ' + paths.ROOT_PATH)
+		project_path = os.path.dirname(paths.ROOT_PATH)
+		print('project_path: %s' % project_path)
+		cfg.set_watch_dir(project_path)
+		cfg.read()
+		ignore = fsmon.should_ignore('/home/logi/repos/testindicator/some.pyc')
+		assert ignore == True
+
+		ignore = fsmon.should_ignore('/home/logi/repos/testindicator/ignore_this_dir/some_file')
+		assert ignore == True
+
+		ignore = fsmon.should_ignore('/home/logi/repos/testindicator/ignore.me')
+		assert ignore == True
+
+		ignore = fsmon.should_ignore('/home/logi/repos/testindicator/dont_ignore.me')
+		assert ignore == False
