65395714ea35be47deb28e1cf35865dd01533944
Logi Leifsson <logantheragon@gmail.com>
2017-12-08 10:39:18 +0000

    cleanup and beautification

--- testindicator/notifier_mac.py
+++ testindicator/notifier_mac.py
@@ -5,7 +5,6 @@ from pync import Notifier as ntfy
 
 # standard library dependencies
 import logging
-import os
 
 # project imports
 from paths import YELLOW_BIG as YELLOW
@@ -16,31 +15,32 @@ import config as cfg
 
 log = logging.getLogger(__name__)
 
+
 class Notifier(object):
-    """Notifier class for testindicator"""
-    def __init__(self):
-        #notify.init('testindicator')
-        #self.notification = None
-        pass
+	"""Notifier class for testindicator"""
+	def __init__(self):
+		#notify.init('testindicator')
+		#self.notification = None
+		pass
 
-    def inform_success(self, msg):
-        log.debug('notify success')
-        self.inform(msg, GREEN)
+	def inform_success(self, msg):
+		log.debug('notify success')
+		self.inform(msg, GREEN)
 
-    def inform_failure(self, msg):
-        log.debug('notify failure')
-        self.inform(msg, RED)
+	def inform_failure(self, msg):
+		log.debug('notify failure')
+		self.inform(msg, RED)
 
-    def inform_unkown(self, msg):
-        log.debug('notify unknown')
-        self.inform(msg, YELLOW)
-        
-    def inform(self, msg, icon=None):
-        if cfg.notifications:
-            title = cfg.project_name
-            #if self.notification is None:
-            ntfy.notify(msg, title=title, appIcon=icon)
-            #else:
-            #    self.notification.update(title, msg, icon)
-            #notification.set_timeout(2)
-            #self.notification.show()
+	def inform_unkown(self, msg):
+		log.debug('notify unknown')
+		self.inform(msg, YELLOW)
+
+	def inform(self, msg, icon=None):
+		if cfg.notifications:
+			title = cfg.project_name
+			#if self.notification is None:
+			ntfy.notify(msg, title=title, appIcon=icon)
+			#else:
+			#    self.notification.update(title, msg, icon)
+			#notification.set_timeout(2)
+			#self.notification.show()
